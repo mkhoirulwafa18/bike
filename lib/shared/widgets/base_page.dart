0f445885c7e1eb025993281dfad3b3c60f30763b
mkhoirulwafa18 <muhkhwafa@gmail.com>
2023-10-16 22:40:24 +0700

    make the appbar optional

--- lib/shared/widgets/base_page.dart
+++ lib/shared/widgets/base_page.dart
@@ -4,20 +4,20 @@ import '../../constants/constant.dart';
 
 class BasePage extends StatelessWidget {
   const BasePage({
-    required this.appBarTitle,
     required this.body,
+    this.appBarTitle,
     this.floatingActionButton,
     super.key,
   }) : padding = const EdgeInsets.all(kSpacingMedium);
 
   const BasePage.noPadding({
-    required this.appBarTitle,
     required this.body,
+    this.appBarTitle,
     this.floatingActionButton,
     super.key,
   }) : padding = EdgeInsets.zero;
 
-  final String appBarTitle;
+  final String? appBarTitle;
   final Widget body;
   final Widget? floatingActionButton;
   final EdgeInsetsGeometry padding;
@@ -28,14 +28,16 @@ class BasePage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: theme.colorScheme.background,
-      appBar: AppBar(
-        title: Text(appBarTitle),
-        titleTextStyle: theme.textTheme.displaySmall,
-        backgroundColor: theme.colorScheme.background,
-        iconTheme: IconThemeData(color: theme.colorScheme.onBackground),
-        elevation: 2,
-        centerTitle: true,
-      ),
+      appBar: appBarTitle != null
+          ? AppBar(
+              title: Text(appBarTitle ?? ''),
+              titleTextStyle: theme.textTheme.displaySmall,
+              backgroundColor: theme.colorScheme.background,
+              iconTheme: IconThemeData(color: theme.colorScheme.onBackground),
+              elevation: 2,
+              centerTitle: true,
+            )
+          : null,
       body: Padding(
         padding: padding,
         child: body,
